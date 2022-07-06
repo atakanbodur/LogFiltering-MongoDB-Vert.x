bfeddd6c78c042880c03e739efa3117741fe464a
atakan <bodur.atakan@gmail.com>
2022-07-06 12:14:45 +0300

    converted to OOP Convention

---
+++ src/main/java/com/foreks/atakanbodur/starter/LogObject.java
@@ -0,0 +1,41 @@
+package com.foreks.atakanbodur.starter;
+
+import io.vertx.core.json.JsonObject;
+
+public class LogObject {
+  private String logData;
+
+  private String returnValueOf(String value) {
+    int index;
+    int nextIndex;
+    index = logData.indexOf(value);
+    if (index == -1)
+      return "null";
+    else {
+      index += (value.length());
+      String substr = logData.substring(index);
+      nextIndex = substr.indexOf(" ");
+      return substr.substring(0, nextIndex);
+    }
+  }
+  public JsonObject initJSONObject() {
+    return new JsonObject()
+      .put("remoteClient", returnValueOf("remoteClient: "))
+      .put("user", returnValueOf("method: "))
+      .put("method", returnValueOf("method: "))
+      .put("statusCode", returnValueOf("statusCode: "))
+      .put("processTimeMS", returnValueOf("processTimeMS: "))
+      .put("protocol", returnValueOf("x-forwarded-proto="))
+      .put("port", returnValueOf("x-forwarded-port="))
+      .put("host", returnValueOf("host="))
+      .put("resource", returnValueOf("resource="))
+      .put("platform", returnValueOf("platform="))
+      .put("appName", returnValueOf("app-name="))
+      .put("appVersion", returnValueOf("app-version="))
+      .put("company", returnValueOf("company="));
+  }
+
+  public void setLogData(String logData) {
+    this.logData = logData;
+  }
+}
